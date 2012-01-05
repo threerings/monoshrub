f473d7cc3da46046e553b4bcd581ddf8d9567d99
Tim Conkling <tconkling@gmail.com>
2012-01-05 15:15:34 -0800

    formattting

--- monoshrub/Point.cs
+++ monoshrub/Point.cs
@@ -10,8 +10,7 @@ public struct Point
     public float x;
     public float y;
 
-    public Point (float x, float y)
-    {
+    public Point (float x, float y) {
         this.x = x;
         this.y = y;
     }
